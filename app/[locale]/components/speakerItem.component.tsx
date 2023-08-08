6ce6957450ed805c987556b6b95af4e24027022b
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-08-08 23:41:57 +0300

    Add Modal to speaker, with callbacks and animations

--- app/[locale]/components/speakerItem.component.tsx
+++ app/[locale]/components/speakerItem.component.tsx
@@ -1,6 +1,9 @@
 "use client"
+import { useState } from "react"
 import DefaultAvatar from "../../../public/DefaultAvatar.jpg"
 import Image from "next/image"
+import SpeakerInformationModal from "./speakerInformationModal.component"
+import { AnimatePresence } from "framer-motion"
 
 interface Props {
     name: string
@@ -9,13 +12,28 @@ interface Props {
 }
 
 export default function SpeakerItem({ name, position, additionalInformation }: Props) {
+    const [showModal, setShowModal] = useState(false);
     return (
-        <div className="flex flex-col ">
-            <div className="grid gap-4 justify-center">
-                <Image className="rounded-full min-h-[25vh] min-w-[65vw] lg:min-h-max lg:min-w-max" src={DefaultAvatar.src} alt="photo" width={150} height={100} />
-                {informationContainer(name, position)}
+        <>
+            <div
+                className="flex flex-col hover:cursor-pointer"
+                onClick={() => setShowModal(true)}>
+                <div className="grid gap-4 justify-center">
+                    <Image className="rounded-full min-h-[25vh] min-w-[65vw] lg:min-h-max lg:min-w-max "
+                        src={DefaultAvatar.src} alt="photo" width={150} height={100} />
+                    {informationContainer(name, position)}
+                </div>
             </div>
-        </div>
+
+            <AnimatePresence>
+                {showModal && (
+                    <SpeakerInformationModal
+                        name={name}
+                        information={additionalInformation}
+                        onClose={() => setShowModal(false)} />
+                )}
+            </AnimatePresence>
+        </>
     )
 }
 
