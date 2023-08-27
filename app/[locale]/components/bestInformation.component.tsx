a95163013784b8b2e6cb1ad768eaed86071eaee9
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-08-27 18:21:22 +0300

    change "about best" component. add styles and new logo

--- app/[locale]/components/bestInformation.component.tsx
+++ app/[locale]/components/bestInformation.component.tsx
@@ -1,24 +1,33 @@
 import { useTranslations } from "next-intl"
 import Image from "next/image";
-import BestLogo from "../../../public/BEST_Lviv.jpg"
+import BestLogo from "../../../public/best_photos/Best_Lviv_logo.png"
+import TextWithLine from "./textWithLine.component";
 
 export default function BestInformation() {
     const t = useTranslations("BestInformation");
     return (
-        <div className="grid lg:grid-cols-2 gap-4 items-center">
-            <div className="text-center px-[10%]">
-                <div className="mb-8 text-left" dangerouslySetInnerHTML={{ __html: t("BEST") }} />
-                <a href="https://best-lviv.org.ua" target="_blank"
-                    className="border rounded-[100px] px-[5vw] py-4 min-h-max
-                            bg-gradient-to-r from-purple-500 to-pink-500
-                          text-white
-                            hover:shadow-2xl">
-                    {t("learn more")}
-                </a>
-            </div>
-            <div className="flex justify-center px-[5%] lg:px-0">
-                <Image src={BestLogo.src} alt="Best logo" width={500} height={150} />
+        // TODO: Add backgroun image "bg-bestLines"
+        <div className="flex flex-col gap-8 lg:gap-12 mt-12">
+            <TextWithLine titleText={t("Organizer")} />
+            <div className="grid lg:grid-cols-2 gap-4 items-center">
+
+                <div className="text-center px-[10%]">
+                    <div className="mb-8 text-left text-l lg:text-[35px] bg-[#0000004D] p-4" dangerouslySetInnerHTML={{ __html: t("BEST") }} />
+
+                    <a href="https://best-lviv.org.ua" target="_blank"
+                        className="border rounded-[100px] px-8 py-3 lg:px-12 lg:py-4 min-h-max
+                            bg-gradient-button text-xl lg:text-[35px]
+                          text-white">
+                        {t("learn more")}
+                    </a>
+                </div>
+
+                <div className="flex justify-center px-[5%] lg:px-0">
+                    <Image src={BestLogo.src} alt="Best logo" width={700} height={350} />
+                </div>
+
             </div>
         </div>
+
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-cyberpunk': 'linear-gradient(to top,#000000,#3840A2,#9800B9,#FFFAFF,#9800B9,#3840A2,#00000000)',
+        'gradient-button': 'linear-gradient(to bottom,  #EC49F7, #9800B9,#3840A2,#2B1543)',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        "bladerunner": "url(/bladerunnerBg.png)"
+        "bladerunner": "url(/bladerunnerBg.png)",
+        "bestLines": "url(/best_photos/bg.png)"
       },
       colors: {
         defaulPurple: "#1a0d28"
