b17d1ceb58d02a4761d7ec7f321730aacd18f222
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-08-27 15:31:07 +0300

    Add gradient border to btwHistory

--- app/[locale]/components/btwHistory.component.tsx
+++ app/[locale]/components/btwHistory.component.tsx
@@ -1,14 +1,16 @@
 import { useTranslations } from "next-intl";
-import TitleText from "./titleText.component";
 import TextWithLine from "./textWithLine.component";
 
 export default function BtwHistory() {
     const t = useTranslations("BTW");
     return (
-        <div className="border-[20px] rounded-[40px] mx-4 lg:border-[40px] lg:rounded-[80px] lg:mx-8 p-4 my-12">
-            <TextWithLine titleText={t("HistoryTitle")} />
+        <div className="bg-gradient-to-b from-[#9800B9] via-[#1900D2] to-[#2E00D1] py-3 px-2 lg:px-6 lg:py-3 mx-4 rounded-[40px] mt-8 lg:mt-24">
+            {/* TODO: Add white border inside div */}
+            <div className="bg-defaulPurple rounded-[40px] px-4 lg:px-0 pt-8 lg:pt-0">
+                <TextWithLine titleText={t("HistoryTitle")} />
 
-            <div className="lg:px-10 lg:text-[30px] text-center justify-center py-4" dangerouslySetInnerHTML={{ __html: t("History") }} />
+                <div className="lg:px-10 lg:text-[30px] text-center lg:text-left justify-center py-4" dangerouslySetInnerHTML={{ __html: t("History") }} />
+            </div>
         </div>
-    )
+    );
 }
