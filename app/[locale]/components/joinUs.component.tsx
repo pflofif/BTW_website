344ee7b715a8ab7372cded5277e113deb4ebdf6c
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-08-31 01:34:48 +0300

    change JoinUs component

--- app/[locale]/components/joinUs.component.tsx
+++ app/[locale]/components/joinUs.component.tsx
@@ -4,13 +4,13 @@ import { useTranslations } from "next-intl";
 
 export default function JoinUs() {
     const t = useTranslations('BTW');
-    const arrowWidth = 50;
-    const arrowHeight = 25;
+    const arrowWidth = 52;
+    const arrowHeight = 27;
 
     return (
-        <div className="flex flex-row gap-8 my-8 items-center mx-4">
-            <Image className="w-52 h-27" src={JoinUsArrow.src} alt="Join Us Right" width={arrowWidth} height={arrowHeight} />
-            <div className="text-center -mx-6">
+        <div className="flex flex-row gap-8 my-8 items-center mx-4 lg:mx-16 lg:my-0">
+            <Image className="w-52 h-27 " src={JoinUsArrow.src} alt="Join Us Right" width={arrowWidth} height={arrowHeight} />
+            <div className="text-center -mx-6 lg:text-[30px] lg:mx-8">
                 {t("JoinUs")}
             </div>
             <Image className="rotate-180 w-52 h-27" src={JoinUsArrow.src} alt="Join Us Right" width={arrowWidth} height={arrowHeight} />
