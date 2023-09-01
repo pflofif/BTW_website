6f4d2a4b50af5dbe6490ad54233d7440f63f8d52
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-09-01 23:07:53 +0300

    Delete uselles code

--- app/[locale]/components/speakerItem.component.tsx
+++ app/[locale]/components/speakerItem.component.tsx
@@ -1,8 +1,5 @@
 "use client"
-import React, { useState } from "react"
 import Image from "next/image"
-import SpeakerInformationModal from "./speakerInformationModal.component"
-import { AnimatePresence } from "framer-motion"
 
 interface Props {
     name: string
@@ -12,26 +9,14 @@ interface Props {
 }
 
 export default function SpeakerItem({ name, position, additionalInformation, srcImage }: Props) {
-    const [showModal, setShowModal] = useState(false);
     return (
-        <>
-            <div onClick={() => setShowModal(true)} className="shadow-[0px_0px_10px_rgba(236,73,247,_1)] bg-defaultPurple border-4 border-black m-4 cursor-pointer">
-                <Image src={srcImage} alt='speaker Image' width={500} height={700} className="px-8 pt-8" />
+        <div className="shadow-[0px_0px_10px_rgba(236,73,247,_1)] bg-defaultPurple border-4 border-black m-4">
+            <Image src={srcImage} alt='speaker Image' width={500} height={700} className="px-8 pt-8" />
 
-                <div className="flex flex-col items-center justify-center pt-8 pb-4">
-                    <span className="text-[25px]">{name}</span>
-                    <span className="text-[20px]">{position}</span>
-                </div>
+            <div className="flex flex-col items-center justify-center pt-8 pb-4">
+                <span className="text-[25px]">{name}</span>
+                <span className="text-[20px]">{position}</span>
             </div>
-
-            <AnimatePresence>
-                {showModal && (
-                    <SpeakerInformationModal
-                        name={name}
-                        information={additionalInformation}
-                        onClose={() => setShowModal(false)} />
-                )}
-            </AnimatePresence>
-        </>
+        </div>
     )
 }
