7edd2e6009aead1489d19af28f3c2310b34c34f0
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-09-14 19:43:02 +0300

    change size

--- app/[locale]/components/speakerItem.component.tsx
+++ app/[locale]/components/speakerItem.component.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function SpeakerItem({ name, position, srcImage }: Props) {
     return (
-        <div className="shadow-[0px_0px_10px_rgba(236,73,247,_1)] bg-defaultPurple border-4 border-black m-4 h-[68vh] lg:h-[85vh]">
+        <div className="shadow-[0px_0px_10px_rgba(236,73,247,_1)] bg-defaultPurple border-4 border-black m-4 h-[68vh] lg:h-[90vh]">
             <Image src={srcImage} alt='speaker Image' width={500} height={700} className="h-[50vh] lg:h-[68vh] px-8 pt-8" />
 
             <div className="flex flex-col items-center justify-center pt-8 pb-4">
